f9cf66e001580c58ca0e4a53175f0ab3444c63fd
codeTukai <tukaighosh753@gmail.com>
2026-06-03 14:41:32 +0530

    fix add Todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -14,15 +14,16 @@ function TodoForm() {
         setTodo("")
     }
   return (
-     <form  className="flex">
+     <form onSubmit={add} className="flex">
             <input
-            onSubmit={add}
-            onChange={(e)=>e.target.value}
+            value={todo}
+            onChange={(e)=>setTodo(e.target.value)}
                 type="text"
+                
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
             />
-            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0 cursor-pointer">
                 Add
             </button>
         </form>
